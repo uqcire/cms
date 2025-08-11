b0d3eca3c38bb67deb789b1c94d99f1b1a0524f7
Chen <ci_re@foxmail.com>
2025-08-11 22:38:52 +1000

    feat(cms): add initial content types  Post, Category, Tag (schemas, base routes)

---
+++ src/api/post/routes/post.js
@@ -0,0 +1,8 @@
+﻿'use strict';
+
+module.exports = {
+  routes: [
+    { method: 'GET', path: '/posts', handler: 'post.find' },
+    { method: 'GET', path: '/posts/:id', handler: 'post.findOne' },
+  ],
+};
